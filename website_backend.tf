9a5d296bff52deb1c5ae965900de7ada765f0c50
snehasharma12 <sharma.sn@northeastern.edu>
2021-08-17 20:53:04 -0400

    fixed snehasharma.tk bug related to region

--- website_backend.tf
+++ website_backend.tf
@@ -159,7 +159,7 @@ resource "aws_lambda_permission" "allow_api_gateway_for_get" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.get_lambda.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:us-east-1:824843761711:${aws_api_gateway_rest_api.contactme_api.id}/*/${aws_api_gateway_method.request_method.http_method}${aws_api_gateway_resource.contactme.path}"
+  source_arn = "arn:aws:execute-api:us-east-2:824843761711:${aws_api_gateway_rest_api.contactme_api.id}/*/${aws_api_gateway_method.request_method.http_method}${aws_api_gateway_resource.contactme.path}"
 
 }
 
@@ -168,7 +168,7 @@ resource "aws_lambda_permission" "allow_api_gateway_for_post" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.post.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:us-east-1:824843761711:${aws_api_gateway_rest_api.contactme_api.id}/*/${aws_api_gateway_method.request_method_post.http_method}${aws_api_gateway_resource.contactme.path}"
+  source_arn = "arn:aws:execute-api:us-east-2:824843761711:${aws_api_gateway_rest_api.contactme_api.id}/*/${aws_api_gateway_method.request_method_post.http_method}${aws_api_gateway_resource.contactme.path}"
 }
 
 #*****************************************************************************************************************
