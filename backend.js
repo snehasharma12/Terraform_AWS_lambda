fbb843bf549e141f11dab686114ffcff9eb7f8de
snehasharma12 <sharma.sn@northeastern.edu>
2021-08-17 20:18:25 -0400

    snehasharma.tk

--- backend.js
+++ backend.js
@@ -7,7 +7,8 @@ var ses = new AWS.SES();
 exports.handler = function (event, context, callback) {
     const params = querystring.parse(event.body);
     
-    var emailParams = {
+    if (params['to'] == 'Sneha'){
+        var emailParams = {
         Destination: {
             ToAddresses: ["sharma.sn@northeastern.edu"]
         },
@@ -25,6 +26,92 @@ exports.handler = function (event, context, callback) {
         },
         Source: "sharma.sn@northeastern.edu"
     };
+    }
+    
+    else if (params['to'] == 'Aron'){
+        var emailParams = {
+        Destination: {
+            ToAddresses: ["dsouza.a@northeastern.edu"]
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: "Name: " + params['name'] + 
+                    "\nEMail: " + params['email'] +
+                    "\nMessage: " + params['messages']
+                }
+            },
+            Subject: {
+                Data: "Message Received from " + params['name']
+            }
+        },
+        Source: "sharma.sn@northeastern.edu"
+    };
+    }
+    
+    else if (params['to'] == 'Tejal'){
+        var emailParams = {
+        Destination: {
+            ToAddresses: ["kadam.t@northeastern.edu"]
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: "Name: " + params['name'] + 
+                    "\nEMail: " + params['email'] +
+                    "\nMessage: " + params['messages']
+                }
+            },
+            Subject: {
+                Data: "Message Received from " + params['name']
+            }
+        },
+        Source: "sharma.sn@northeastern.edu"
+    };
+    }
+    
+     
+    else if (params['to'] == 'Ashish'){
+        var emailParams = {
+        Destination: {
+            ToAddresses: ["sateesha.a@northeastern.edu"]
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: "Name: " + params['name'] + 
+                    "\nEMail: " + params['email'] +
+                    "\nMessage: " + params['messages']
+                }
+            },
+            Subject: {
+                Data: "Message Received from " + params['name']
+            }
+        },
+        Source: "sharma.sn@northeastern.edu"
+    };
+    }
+    
+    else if (params['to'] == 'Sanyukta'){
+        var emailParams = {
+        Destination: {
+            ToAddresses: ["koli.s@northeastern.edu"]
+        },
+        Message: {
+            Body: {
+                Text: {
+                    Data: "Name: " + params['name'] + 
+                    "\nEMail: " + params['email'] +
+                    "\nMessage: " + params['messages']
+                }
+            },
+            Subject: {
+                Data: "Message Received from " + params['name']
+            }
+        },
+        Source: "sharma.sn@northeastern.edu"
+    };
+    }
     
     ses.sendEmail(emailParams, function(err, data) {
         if (err) console.log(err, err.stack); // an error occurred
@@ -36,7 +123,7 @@ exports.handler = function (event, context, callback) {
         headers: { 'Access-Control-Allow-Origin': 'http://snehasharma.tk' },
         body: JSON.stringify( 'Thank you, ' + params ['name'] + '! ' + 
                              'Your message is received by ' + params ['to'] + '! ' +
-                             'She will contact you soon!! '),
+                             'you will be contacted soon!! '),
     };
     callback(null, response);
 };
